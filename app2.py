7ff42b1b24e755f8a59f091fad40df76956cf84b
ofischbach24 <139179568+ofischbach24@users.noreply.github.com>
2024-02-10 17:59:37 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from colorama import Fore
-from motorlib import board, motor
 import RPi.GPIO as GPIO
+from motorlib import board, motor
 from evdev import InputDevice, ecodes
 from inputs import get_gamepad
 
@@ -9,12 +9,17 @@ from inputs import get_gamepad
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Setup Boards
-pi = board('pi', type="pi")
-
 # Setup Motors
-RightTread = motor(pi, pins=[1, 11])
-LeftTread = motor(pi, pins=[2, 12])
+LEFT_TREAD_PINS = [2, 12]
+RIGHT_TREAD_PINS = [1, 11]
+
+GPIO.setup(LEFT_TREAD_PINS[0], GPIO.OUT)
+GPIO.setup(LEFT_TREAD_PINS[1], GPIO.OUT)
+GPIO.setup(RIGHT_TREAD_PINS[0], GPIO.OUT)
+GPIO.setup(RIGHT_TREAD_PINS[1], GPIO.OUT)
+
+left_tread_pwm = GPIO.PWM(LEFT_TREAD_PINS[0], 100)
+right_tread_pwm = GPIO.PWM(RIGHT_TREAD_PINS[0], 100)
 
 # Setup Power Variables
 powerDrive = 1
@@ -25,13 +30,37 @@ DEADZONE_THRESHOLD = 0.2
 def apply_deadzone(value, threshold):
     return 0 if abs(value) < threshold else value
 
-def Shutdown(message):
-    if message:
-        # Sets all motors to 0
-        RightTread.start(0)
-        LeftTread.start(0)
+def motor_control(motor_pwm, joystick_value):
+    motor_pwm.start(int(joystick_value * 100 * powerDrive))
 
-        print(Fore.RED + 'Shutdown' + Fore.RESET)
+def shutdown():
+    GPIO.cleanup()
+    print(Fore.RED + 'Shutdown' + Fore.RESET)
+
+# Main loop to continuously check gamepad input
+if __name__ == '__main__':
+    print(Fore.RED + 'Server started' + Fore.RESET)
+    try:
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.ev_type == 'Absolute' and event.ev_code == 'ABS_Y':
+                    # Left stick vertical axis (up and down)
+                    left_stick_y = event.ev_value / 32767.0
+                    left_stick_y = apply_deadzone(left_stick_y, DEADZONE_THRESHOLD)
+                    motor_control(left_tread_pwm, left_stick_y)
+
+                elif event.ev_type == 'Absolute' and event.ev_code == 'ABS_RY':
+                    # Right stick vertical axis (up and down)
+                    right_stick_y = event.ev_value / 32767.0
+                    right_stick_y = apply_deadzone(right_stick_y, DEADZONE_THRESHOLD)
+                    motor_control(right_tread_pwm, right_stick_y)
+
+                elif event.ev_type == 'Key' and event.ev_code == 'BTN_START' and event.ev_value == 1:
+                    shutdown()
+
+    except KeyboardInterrupt:
+        shutdown()
 
 # Path to the event file for the gamepad, adjust as needed
 gamepad_path = "/dev/input/eventX"  # Replace eventX with the correct event file
@@ -43,16 +72,16 @@ try:
             if event.code == ecodes.ABS_Y:
                 joystick1_value = event.value / 32767.0
                 joystick1_value = apply_deadzone(joystick1_value, DEADZONE_THRESHOLD)
-                LeftTread.start(int(joystick1_value * 100 * powerDrive))
+                motor_control(left_tread_pwm, joystick1_value)
 
             elif event.code == ecodes.ABS_RX:
                 joystick2_value = event.value / 32767.0
                 joystick2_value = apply_deadzone(joystick2_value, DEADZONE_THRESHOLD)
-                RightTread.start(int(joystick2_value * 100 * powerDrive))
+                motor_control(right_tread_pwm, joystick2_value)
 
         elif event.type == ecodes.EV_KEY and event.code == ecodes.BTN_START:
             shutdown_value = event.value
-            Shutdown(shutdown_value)
+            shutdown()
 
 except KeyboardInterrupt:
     GPIO.cleanup()
